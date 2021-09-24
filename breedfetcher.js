df8fc53fb754bb00e8e89c296dfaab7ae06e531e
faridAbinah <faridabinah@hotmail.com>
2021-09-23 20:11:35 -0400

    Created breed fetcher test file which will run automated tests using mocha and chai

--- breedfetcher.js
+++ breedfetcher.js
@@ -1,27 +1,37 @@
 const request = require('request');
 
-const args = process.argv.slice(2);
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${args}`
 
+
+
+
+const fetchBreedDescription = function(breedName, callback) {
+
+  
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,  (err, res, body) => {
+    if (err) {
+      callback(err,null);
+   
+    }
+    const data = JSON.parse(body);
+    const breed = data[0];
+  
+
+    if (breed) {
+      callback(null,data[0].description);
+  } else {
+    callback("Breed Not Found");
+  }
+  
+  
+});
+};
+module.exports = {fetchBreedDescription};
 
 // console.log(url);
 // console.log(request.get(url));
 // request(url);
 //Here we are allowing the user to specify the breed name using command line arguments by accepting the command line arguments, storing them in our args variable using process.argv.slive and then passing that to our request function as the first parameter. With the use of a template literal we are able to dynamically pass user input to our request so the user can access any breed.
 //The body is the entire response sent back from the API Server.
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`,  (err, res, body) => {
-  if (err) { return console.log(err); }
-  //console.log(body)
-  
-  const data = JSON.parse(body)
-  //console.log(data[0]);
-  //Here we are checking if data[0] is empty or if there was nothing retrieved from our request.
-  if(!data[0]) {
-    console.log("Sorry there is an error");
+// The description is fetched via an API request, which is network I/O. We use request which uses good Node practice by fetching our results asynchronously. Therefore, our fetch function also needs to be asynchronous. It should therefore accept a callback. We talked about how asynchronous functions can't simply return data in the previous cat exercise.
 
-    return;
-  }
 
-  console.log(`${data[0].name}\nweight: ${data[0].weight.imperial}\ndescription: ${data[0].temperament}`); 
-  
-});
---
+++ test/breedFetcherTest.js
@@ -0,0 +1,46 @@
+const { fetchBreedDescription } = require('../breedfetcher');
+const { assert } = require('chai');
+
+describe('fetchBreedDescription', () => {
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      // we expect no error for this scenario
+      assert.equal(err, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+
+      // compare returned description
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+  });
+
+    it('returns error message if breed is invalid', (done) => {
+      fetchBreedDescription('BMW', (err, desc) => {
+        
+       
+  
+        const expectedError = "Breed Not Found";
+  
+    
+        assert.equal(expectedError,err);
+        assert.equal(desc,null);
+  
+  
+        done();
+
+      
+      });
+   
+});
+
+ 
+     
+      
+    
+   
+
+});
+
+
